c750fd84f462490694ceac2d1b62fdd0f7a41eaa
doogunwo <dgw0601@naver.com>
2024-07-01 21:25:54 +0900

    test passed

--- memtable/memtable-test.go
+++ memtable/memtable-test.go
@@ -1,1 +1,21 @@
+package memtable
 
+import (
+  "fmt"
+  "testing"
+)
+
+func Testmemdb(t *testing.T){
+  memtable := NewMemTable()
+
+  memtable.Put("key1", []byte("v1"))
+  memtable.Put("key1", []byte("v2"))
+
+  val, exists := memtable.Get("key1")
+  if !exists || string(val) != "v1" {
+    t.Errorf("v1 for k1 for %s", string(val))
+  }
+  
+  fmt.Println("passed")
+
+}
